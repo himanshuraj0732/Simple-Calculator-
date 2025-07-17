e70dd6b89f538da568f66e9c7334c4619a16ca0d
himanshuraj0732 <hraj33200@gmail.com>
2025-07-17 16:37:12 +0530

    Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,24 @@
+# Simple Calculator in Python
+
+print("Welcome to Simple Calculator")
+print("Operations: +  -  *  /")
+
+num1 = float(input("Enter first number: "))
+op = input("Enter operator (+, -, *, /): ")
+num2 = float(input("Enter second number: "))
+
+if op == '+':
+    result = num1 + num2
+elif op == '-':
+    result = num1 - num2
+elif op == '*':
+    result = num1 * num2
+elif op == '/':
+    if num2 != 0:
+        result = num1 / num2
+    else:
+        result = "Error! Cannot divide by zero."
+else:
+    result = "Invalid operator"
+
+print("Result:", result)
